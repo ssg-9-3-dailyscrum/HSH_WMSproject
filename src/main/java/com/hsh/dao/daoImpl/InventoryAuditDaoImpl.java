8a846c6c562c5178f8fd9c31019e0d68bf3501e4
GYULIM <gyuliming@gmail.com>
2025-09-25 16:58:13 +0900

    feat: 재고관리 DAO 추가 및 VO 수정

---
+++ src/main/java/com/hsh/dao/daoImpl/InventoryAuditDaoImpl.java
@@ -0,0 +1,18 @@
+package main.java.com.hsh.dao.daoImpl;
+
+public class InventoryAuditDaoImpl {
+    @Override
+    public void executeInventoryAudit(int warehouseId) {
+
+    }
+
+    @Override
+    public List<InventoryAuditVo> selectWarehouseAuditList(int warehouseId) {
+        return null;
+    }
+
+    @Override
+    public List<InventoryAuditVo> selectSectionAuditList(int sectionId) {
+        return null;
+    }
+}
