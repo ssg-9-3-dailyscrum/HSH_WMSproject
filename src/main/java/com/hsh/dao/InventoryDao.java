5bf53f9fb3743098cdbcee9705f0a5f1653e4600
GYULIM <gyuliming@gmail.com>
2025-09-25 17:15:58 +0900

    update: 주석처리

--- src/main/java/com/hsh/dao/InventoryDao.java
+++ src/main/java/com/hsh/dao/InventoryDao.java
@@ -1,9 +1,9 @@
-package main.java.com.hsh.dao;
-
-public interface InventoryDao {
-    List<InventoryVo> selectAllInventory(int warehouseId);
-    List<CategoryInventoryVo> selectTopCategoryInventory(int warehouseId, int categoryDepth);
-    List<SubCategoryInventoryVo> selectSubCategoryInventory(int warehouseId, int categoryDepth);
-    ProductVo selectProductDetail(int productId);
-    List<InventoryVo> selectWarehouseInventory(int warehouseId);
-}
+//package main.java.com.hsh.dao;
+//
+//public interface InventoryDao {
+//    List<InventoryVo> selectAllInventory(int warehouseId);
+//    List<CategoryInventoryVo> selectTopCategoryInventory(int warehouseId, int categoryDepth);
+//    List<SubCategoryInventoryVo> selectSubCategoryInventory(int warehouseId, int categoryDepth);
+//    ProductVo selectProductDetail(int productId);
+//    List<InventoryVo> selectWarehouseInventory(int warehouseId);
+//}
