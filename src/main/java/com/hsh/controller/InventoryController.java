64af86092b6d9c75ea6772c91eab0e145d19d8bd
jaehoon0321 <jaehoon0321@gmail.com>
2025-09-29 10:18:37 +0900

    update : 주석 제거 2025_09_29LJH

--- src/main/java/com/hsh/controller/InventoryController.java
+++ src/main/java/com/hsh/controller/InventoryController.java
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-//package main.java.com.hsh.controller;
-//
-//import main.java.com.hsh.domain.dto.response.InventoryResponse;
-//import main.java.com.hsh.domain.dto.response.ProductResponse;
-//import main.java.com.hsh.domain.dto.response.WarehouseStatusResponse;
-//import main.java.com.hsh.service.InventoryService;
-//import main.java.com.hsh.service.seviceImpl.InventoryServiceImpl;
-//import main.java.com.hsh.util.UserSession;
-//
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//
-//public class InventoryController {
-//    private static InventoryController instance = new InventoryController();
-//    private InventoryService inventoryService;
-//
-//    public InventoryController() {
-//        this.inventoryService = InventoryServiceImpl.getInstance();
-//    }
-//
-//    // 전역 접근 지점 (항상 같은 인스턴스를 반환)
-//    public static InventoryController getInstance() {
-//        return instance; // 이미 있으면 기존 객체 반환
-//    }
-//
-//
-//    // 전체 재고 조회
-//    public List<InventoryResponse> showAllInventory() {
-//        String userRole = UserSession.getCurrentUserRole();
-//        Integer userId = UserSession.getCurrentUserId();
-//        return inventoryService.getAllInventory(userRole, userId);
-//    }
-//
-////    // 대분류, 소분류 리스트
-////    public List<String> showTopCategoryList() throws SQLException {
-////        return inventoryService.getTopCategoryList(userRole, userId);
-////    }
-////
-////    public List<String> showSubCategoryList() throws SQLException {
-////        return inventoryService.getSubCategoryList(userRole, userId, category);
-////    }
-////
-////    public List<InventoryResponse> showTopCategoryInventory() {
-////        return inventoryService.getCategoryInventory(userRole, userId);
-////    }
-//
-//    // 상품 상세 조회
-//    public List<ProductResponse> showProductDetail(String productName) {
-//        String userRole = UserSession.getCurrentUserRole();
-//        Integer userId = UserSession.getCurrentUserId();
-//        return inventoryService.getProductDetail(userRole, userId, productName);
-=======
 package main.java.com.hsh.controller;
 
 import main.java.com.hsh.domain.dto.response.InventoryResponse;
@@ -118,19 +64,19 @@ public class InventoryController {
         return inventoryService.getWarehouse();
     }
 
+    public List<InventoryResponse> showInventoryAuditLog() {
+        System.out.println("재고 실사 조회 기능 준비 중입니다.");
+
+    }
+
+    // 창고 현황 조회
+    public List<WarehouseStatusResponse> showWarehouse() {
+        return inventoryService.getWarehouse();
+    }
+
 //    public List<InventoryResponse> showInventoryAuditLog() {
 //        System.out.println("재고 실사 조회 기능 준비 중입니다.");
->>>>>>> develop
 //    }
-//
-//    // 창고 현황 조회
-//    public List<WarehouseStatusResponse> showWarehouse() {
-//        return inventoryService.getWarehouse();
-//    }
-//
-////    public List<InventoryResponse> showInventoryAuditLog() {
-////        System.out.println("재고 실사 조회 기능 준비 중입니다.");
-////    }
-//
-//
-//}
+
+
+}
